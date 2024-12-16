ca4f917500faa1f687b9eda21bd64233f8d6e03a
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2024-12-16 21:08:21 +0200

    Rezolvare Laborator 9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using SQLite;
+﻿using SQLite;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using CiciovanBogdanIonutLab7.Models;
 
@@ -11,35 +8,88 @@ namespace CiciovanBogdanIonutLab7.Data
     public class ShoppingListDatabase
     {
         readonly SQLiteAsyncConnection _database;
+
         public ShoppingListDatabase(string dbPath)
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
+
+        public Task<int> SaveShopListAsync(ShopList shopList)
+        {
+            if (shopList.ID != 0)
+            {
+                return _database.UpdateAsync(shopList);
+            }
+            else
+            {
+                return _database.InsertAsync(shopList);
+            }
+        }
+
+        public Task<int> DeleteShopListAsync(ShopList shopList)
+        {
+            return _database.DeleteAsync(shopList);
+        }
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
         }
-        public Task<ShopList> GetShopListAsync(int id)
+
+        public Task<int> SaveProductAsync(Product product)
         {
-            return _database.Table<ShopList>()
-            .Where(i => i.ID == id)
-           .FirstOrDefaultAsync();
-        }
-        public Task<int> SaveShopListAsync(ShopList slist)
-        {
-            if (slist.ID != 0)
+            if (product.ID != 0)
             {
-                return _database.UpdateAsync(slist);
+                return _database.UpdateAsync(product);
             }
             else
             {
-                return _database.InsertAsync(slist);
+                return _database.InsertAsync(product);
             }
         }
-        public Task<int> DeleteShopListAsync(ShopList slist)
+
+        public Task<int> DeleteProductAsync(Product product)
         {
-            return _database.DeleteAsync(slist);
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listProduct)
+        {
+            if (listProduct.ID != 0)
+            {
+                return _database.UpdateAsync(listProduct);
+            }
+            else
+            {
+                return _database.InsertAsync(listProduct);
+            }
+        }
+
+        public Task<int> DeleteListProductAsync(ListProduct listProduct)
+        {
+            return _database.DeleteAsync(listProduct);
+        }
+
+        public Task<List<Product>> GetListProductsAsync(int shoplistId)
+        {
+            return _database.QueryAsync<Product>(
+                "SELECT P.ID, P.Description FROM Product P " +
+                "INNER JOIN ListProduct LP ON P.ID = LP.ProductID " +
+                "WHERE LP.ShopListID = ?", shoplistId);
+        }
+
+        public Task<ListProduct> GetListProductAsync(int shoplistId, int productId)
+        {
+            return _database.Table<ListProduct>()
+                .FirstOrDefaultAsync(lp => lp.ShopListID == shoplistId && lp.ProductID == productId);
         }
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,24 +1,63 @@
+using System;
 using CiciovanBogdanIonutLab7.Models;
+using Microsoft.Maui.Controls;
 
-namespace CiciovanBogdanIonutLab7;
-
-public partial class ListPage : ContentPage
+namespace CiciovanBogdanIonutLab7
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
-    async void OnSaveButtonClicked(object sender, EventArgs e)
+    public partial class ListPage : ContentPage
     {
-        var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
-        await Navigation.PopAsync();
-    }
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
-    {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopList = (ShopList)this.BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var shopList = (ShopList)BindingContext;
+            await App.Database.SaveShopListAsync(shopList);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var shopList = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(shopList);
+            await Navigation.PopAsync();
+        }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            var shopList = (ShopList)this.BindingContext;
+            await Navigation.PushAsync(new ProductPage(shopList)
+            {
+                BindingContext = new Product()
+            });
+        }
+
+        async void OnDeleteItemClicked(object sender, EventArgs e)
+        {
+            var selectedProduct = listView.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                var shopList = (ShopList)this.BindingContext;
+                var listProduct = await App.Database.GetListProductAsync(shopList.ID, selectedProduct.ID);
+                if (listProduct != null)
+                {
+                    await App.Database.DeleteListProductAsync(listProduct);
+                    listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+                }
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please select an item to delete.", "OK");
+            }
+        }
     }
 }
