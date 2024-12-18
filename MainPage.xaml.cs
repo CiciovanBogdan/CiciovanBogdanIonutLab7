e5e659fe46b86699a581f266271fadc482ad92fe
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2024-12-18 18:32:58 +0200

    Rezolvare finala lab 10

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,8 +2,6 @@
 {
     public partial class MainPage : ContentPage
     {
-        //int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -12,18 +10,6 @@
         {
             DisplayAlert("Informații suplimentare", "Aceasta este pagina principală personalizată pentru laboratorul 7.", "OK");
         }
-
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
-
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
     }
 
 }
